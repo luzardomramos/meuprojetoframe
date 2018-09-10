66a6b8100697e12a3ad708dae4faafdeda712e31
aluno <aluno@GRVL301B-15>
2018-09-10 20:45:10 -0300

    236

--- application/controllers/Contato.php
+++ application/controllers/Contato.php
@@ -33,7 +33,7 @@ class Contato extends CI_Controller {
 
     public function editar($id) {
     $this->load->view('template/header');
-//    $data['acronico'] = "MPF";    
+    $data['acronico'] = "MPF";    
     $data['completo'] = "Meu Projeto Frame";    
     $data['contatoEditar'] = $this->contatos->editar($id);
     $this->load->view('contatoEditar', $data);   
