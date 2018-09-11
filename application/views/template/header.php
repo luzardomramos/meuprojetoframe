4c21785936fc8bb83849e438bebf5eab723b517b
aluno <aluno@GRVL301B-15>
2018-09-10 21:54:01 -0300

    237

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -7,11 +7,14 @@ and open the template in the editor.
 
     <html>
     <head>
-        <!--       <meta charset="UTF-8">-->
+<!--               <meta charset="UTF-8">-->
         <title>Meu Projeto FrameWork</title>
         <!--        Bootstrap-->
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet"/>
         <link href="<?php echo base_url('assets/css/bootstrap-grid.min.css') ?>" rel="stylesheet"/>
+        <!--Datatable-->
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
+  
     </head>
     <body>
