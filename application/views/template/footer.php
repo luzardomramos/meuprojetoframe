0f0e6092acfe919949e7e885fe6eac65d84c9071
aluno <aluno@GRVL301B-15>
2018-09-12 19:44:23 -0300

    237

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -8,6 +8,7 @@ and open the template in the editor.
 
 <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>" ></script>
 <script type="text/javascript"  src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+<script type="text/javascript"  src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
 
 </body>
 </html>
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -5,16 +5,19 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 
-    <html>
+<html>
     <head>
-<!--               <meta charset="UTF-8">-->
+        <!--               <meta charset="UTF-8">-->
         <title>Meu Projeto FrameWork</title>
         <!--        Bootstrap-->
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet"/>
         <link href="<?php echo base_url('assets/css/bootstrap-grid.min.css') ?>" rel="stylesheet"/>
         <!--Datatable-->
-        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
-  
+        <!--<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">-->
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jq-3.3.1/jszip-2.5.0/dt-1.10.18/af-2.3.0/b-1.5.2/b-flash-1.5.2/b-html5-1.5.2/b-print-1.5.2/r-2.2.2/datatables.min.js"></script>
+        
     </head>
     <body>
